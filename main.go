7de03effb86d52d9516d29ab6826272a3dde3b88
Laércio Lopes <LaercioLopesLL@users.noreply.github.com>
2026-04-21 21:47:11 -0300

    add main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "A hora atual é: %s", time.Now().Format("2006-01-02 15:04:05"))
+	})
+	http.ListenAndServe(":8080", nil)
+}
